d02562f179718a1fac1380fbfdf91ff22155d887
William Andres Rodriguez Borja <williamzborja@gmail.com>
2024-08-27 14:15:59 -0500

    Revert "Feature/data"

--- food-truck/src/app/(dashboard)/page.tsx
+++ food-truck/src/app/(dashboard)/page.tsx
@@ -1,17 +1,13 @@
 import { ReactNode } from 'react';
 import FoodTrackTable from './food-track-table';
-import prisma from '@/lib/db';
 
 
 
-export default async function Page({
+export default function Page({
   children
 }: {
   children: ReactNode;
 }) {
-  const foodTrucks = await prisma.mobileFoodFacilityPermit.count();
-
-  console.log(foodTrucks);
   return (
     <FoodTrackTable />
   );
