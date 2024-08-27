3531e5def6a700b05a1d758e524cc0d48e152652
william <williamzborja@gmail.com>
2024-08-27 16:16:21 -0500

    finish project

--- food-truck/src/app/(dashboard)/page.tsx
+++ food-truck/src/app/(dashboard)/page.tsx
@@ -1,24 +1,19 @@
 
+import { Suspense } from 'react';
 import FoodTrackTable from './food-track-table';
-import prisma from '@/lib/db';
 
+type Props = {
+  searchParams: {
+    query?: string;
+    page?: number;
+  };
+};
 
-export default async function Page({
-  children
-}: {
-  children: ReactNode;
-}) {
-  const foodTrucks = await prisma.mobileFoodFacilityPermit.count();
-
-  console.log(foodTrucks);
+export default async function Page({ searchParams: { q, page } }: Props) {
+  const query = q ?? '';
+  page = page ?? 0;
   return (
     <div>
-      <div className='flex justify-between my-4 '>
-        <div>
-
-        </div>
-      </div>
-
       <Suspense fallback={<div>Loading...</div>}>
         <FoodTrackTable query={query} page={page} />
       </Suspense>
